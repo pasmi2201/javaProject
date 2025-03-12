c3b6483c67cec998713a86f7c2b25d4caa562084
pasmi2201 <arockiapasmi@gmail.com>
2025-03-12 14:03:28 +0530

    I have completed my first program

--- variable/Example.java
+++ variable/Example.java
@@ -1,14 +1,8 @@
 public class Example
 {
-    public void Add(int num1,int num2)
-    {
-        int num3=num1+num2;
-        System.out.println(num3);
-    }
     public static void main(String args[])
     {
-        Example obj=new Example();
-        obj.Add(20,30);
+        int num=10;
+        System.out.println(num);
     }
-    
 }
