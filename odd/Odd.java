d46c124fb7fca8540c0068dfd5f1a48e44770246
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 14:20:31 +0530

    I had completed my odd program

---
+++ odd/Odd.java
@@ -0,0 +1,11 @@
+public class Odd
+{
+    public static void main(String args[])
+    {
+        for(int i=27;i<=38;i++)
+        if(i%2!=0)
+        {
+            System.out.println(i);
+        }
+    }
+}
