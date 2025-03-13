c22fdabddaf2935c560636c03d306e7bcadd49ab
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 10:22:06 +0530

    I had done my arithmetic operator program

---
+++ arithmeticOperator/Arithmetic1.java
@@ -0,0 +1,11 @@
+public class Arithmetic1
+{
+    public static void main(String args[])
+    {
+        System.out.println(10+5);
+        System.out.println(20-13);
+        System.out.println(34*2);
+        System.out.println(25/5);
+        System.out.println(65%23);
+    }
+}
