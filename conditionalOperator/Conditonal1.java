43e2cf3c1c0ed29b29c819a543ba57b4915f105f
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 10:33:10 +0530

    I had done my conditional program

---
+++ conditionalOperator/Conditonal1.java
@@ -0,0 +1,12 @@
+public class Conditonal1
+{
+    public static void main(String args[])
+    {
+        System.out.println(23<10);
+        System.out.println(23>10);
+        System.out.println(23<=12);
+        System.out.println(23>=10);
+        System.out.println(23==23);
+        System.out.println(23!=23);
+    }
+}
