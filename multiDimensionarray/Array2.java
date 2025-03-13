b94d2795de29670b87a93fc0fcd149ab1414a266
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 13:39:51 +0530

    Ihad completed my multi dimension array program

---
+++ multiDimensionarray/Array2.java
@@ -0,0 +1,12 @@
+public class Array2
+{
+    public static void main(String args[])
+    {
+        int a[][]={{10,20},{40,50}};
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+
+    }
+}
