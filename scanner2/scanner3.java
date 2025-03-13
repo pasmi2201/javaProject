364a5d4cbff3ca4849854fd8ada9ff32232fdd58
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 15:16:31 +0530

    I had done my scanner int program

---
+++ scanner2/scanner3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class scanner3
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+    }
+}
