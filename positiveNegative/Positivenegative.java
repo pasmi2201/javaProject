51cf2ead558004ff5a712db417b19be11dfdae9b
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 11:44:13 +0530

    I had modify the program

--- positiveNegative/Positivenegative.java
+++ positiveNegative/Positivenegative.java
@@ -2,10 +2,14 @@ public class Positivenegative
 {
     public static void main (String args[])
     {
-        int num=-20;
-        if(num>=0)
+        int num=0;
+        if(num>0)
         {
             System.out.println("The number is positive");
+        }
+        else if(num==0)
+        {
+            System.out.println("The number is mutual number");
         }
         else{
             System.out.println("The number is negative");
