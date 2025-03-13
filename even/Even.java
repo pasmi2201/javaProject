e31de6dfd04f1eee28a0fc128e570bab9699f98b
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 14:15:20 +0530

    I had done my even program'

---
+++ even/Even.java
@@ -0,0 +1,12 @@
+public class Even
+{
+    public static void main(String args[])
+    {
+        for(int i=11;i<=20;i++)
+        if(i%2==0)
+        {
+            System.out.println(i);
+        }
+        
+    }
+}
