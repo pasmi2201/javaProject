325d1cb4a27f7e9488afa8ee2a93688d218501b0
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 11:36:25 +0530

    I had done my gender find program

---
+++ gender/Gender.java
@@ -0,0 +1,14 @@
+public class Gender
+{
+    public static void main (String args[])
+    {
+        char gen='M';
+        if(gen=='M')
+        {
+            System.out.println("The gender is Male");
+        }
+        else{
+            System.out.println("The gender is female");
+        }
+    }
+}
