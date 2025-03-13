5b029c782295055876103d4c3817e8265c934670
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 14:26:01 +0530

    I had completed my leap year program

---
+++ leapYear/Leapyear.java
@@ -0,0 +1,14 @@
+public class Leapyear
+{
+    public static void main(String args[])
+    {
+        int year=2025;
+        if(year%4==0)
+        {
+            System.out.println("The given year is leap year");
+        }
+        else{
+            System.out.println("It is not leap year");
+        }
+    }
+}
