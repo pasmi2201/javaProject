19b47596a9aff3a53b96f5e6b153357f77563312
pasmi2201 <arockiapasmi@gmail.com>
2025-03-13 11:12:10 +0530

    I had done my positive negative program

---
+++ majorMinorfinder/Magorminor.java
@@ -0,0 +1,14 @@
+public class Magorminor
+{
+    public static void main (String args[])
+    {
+        int num=20;
+        if(num>=18)
+        {
+            System.out.println("the age is major age");
+        }
+        else{
+            System.out.println("The age is minor age");
+        }
+    }
+}
---
+++ positiveNegative/Positivenegative.java
@@ -0,0 +1,14 @@
+public class Positivenegative
+{
+    public static void main (String args[])
+    {
+        int num=-20;
+        if(num>=0)
+        {
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative");
+        }
+    }
+}
