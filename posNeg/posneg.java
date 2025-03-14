f65436b03c9ba67678eb333418f7587dae97b4ec
pasmi2201 <arockiapasmi@gmail.com>
2025-03-14 09:35:39 +0530

    I had done my second if else program

---
+++ posNeg/posneg.java
@@ -0,0 +1,15 @@
+public class posneg
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        if(num>=0)
+        {
+            System.out.println("positive");
+        }
+        else
+        {
+            System.out.println("negative");
+        }
+    }
+}
