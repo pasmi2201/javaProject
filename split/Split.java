1fe1b952de4e3033e1e97d59a2f580f708865fa4
pasmi2201 <arockiapasmi@gmail.com>
2025-03-14 11:50:44 +0530

    I had done my split program

---
+++ split/Split.java
@@ -0,0 +1,10 @@
+public class Split
+{
+    public static void main(String args[])
+    {
+        String name="Minato,Kushina";
+        String naruto[]=name.split(",");
+        System.out.println(naruto[0]);
+        System.out.println(naruto[1]);
+    }
+}
