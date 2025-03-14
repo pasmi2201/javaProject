f5d43585fd5d09716961b070c42575b8673d8814
pasmi2201 <arockiapasmi@gmail.com>
2025-03-14 12:01:57 +0530

    I had done my lower case upper case program

---
+++ cases/Case.java
@@ -0,0 +1,9 @@
+public class Case
+{
+    public static void main(String args[])
+    {
+        String name="Itachi Uchiha";
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+    }
+}
