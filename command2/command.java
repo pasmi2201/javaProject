b5d7b9343bfac8aa7f9e316c8ecd14f8c6b3ee66
pasmi2201 <arockiapasmi@gmail.com>
2025-03-14 09:45:27 +0530

    I had done my multiple command program

---
+++ command2/command.java
@@ -0,0 +1,12 @@
+public class command
+{
+    /*
+        1)demo files
+        2)using variables
+    */
+    public static void main(String args[])
+    {
+        int num=10;
+        System.out.println(num);
+    }
+}
