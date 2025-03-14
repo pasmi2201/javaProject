da5a8534377c58d0b64b8151f7dc575045774e70
pasmi2201 <arockiapasmi@gmail.com>
2025-03-14 10:11:47 +0530

    I had done my 7th table program

---
+++ table/Table.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Table
+{
+    public static void main(String args[])
+    {
+        int i=7;
+        int x=1;
+        while(i<=70)
+        {
+            System.out.println(x+" "+"x 7="+i);
+            x=x+1;
+            i=i+7;
+        }
+    }
+}
