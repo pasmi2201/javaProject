1dbeff11a0b4fa388330f368ddf8b030d034d57c
pasmi2201 <arockiapasmi@gmail.com>
2025-03-14 10:02:13 +0530

    I had done my While program

---
+++ while/While1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class While1
+{
+    public static void main(String args[])
+    {
+        int i=92;
+        while(i<99)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
