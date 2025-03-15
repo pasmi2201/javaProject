aa409db2c807e3b236dd2eba928668db548bbf6e
pasmi2201 <arockiapasmi@gmail.com>
2025-03-15 10:11:45 +0530

    I had done my second leap year program

---
+++ leap/Leap.java
@@ -0,0 +1,13 @@
+public class Leap
+{
+    public static void main(String args[])
+    {
+        for(int i=2020;i<=2030;i++)
+        if(i%4==0)
+        {
+            System.out.println(i);
+        
+        }
+        
+    }
+}
