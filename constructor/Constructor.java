baf6a06c97ba5f68495dbd45c1952f4ee747e55b
pasmi2201 <arockiapasmi@gmail.com>
2025-03-15 10:36:06 +0530

    I had done my constructor program

---
+++ constructor/Constructor.java
@@ -0,0 +1,12 @@
+public class Constructor
+{
+    Constructor()
+    {
+        System.out.println("Gaara");
+    }
+    public static void main(String args[])
+    {
+        Constructor sand=new Constructor();
+        
+    }
+}
