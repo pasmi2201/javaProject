292e9e3c4e05d482df8e7a379b962b87c1389b2a
pasmi2201 <arockiapasmi@gmail.com>
2025-03-15 11:06:47 +0530

    I had done my salary program

---
+++ salaryGenerator/Salary.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Salary
+{
+     public static void main(String args[])
+     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\n1.Developer\n2.Manager");
+        System.out.println("Enter the chice:");
+        int i=sc.nextInt();
+        System.out.println("Enter the slary:");
+        int salary=sc.nextInt();
+        if(i==1)
+        {
+            double percent=salary*0.1;
+            double salary1=salary+percent;
+            System.out.println("The salary is:"+salary1);
+        }
+        else if(i==2)
+        {
+            double percent2=salary*0.2;
+            double salary2=salary+percent2;
+            System.out.println("The salary is:"+salary2);
+        }
+     }
+}
